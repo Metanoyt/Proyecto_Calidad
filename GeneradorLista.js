bbb81853bba731f0ee0187dd2dd52bd246766a31
JuanBautistaF <hidalgojb.23@gmail.com>
2021-10-27 17:36:35 -0600

    Correccion de modulos para el feature 2

--- GeneradorLista.js
+++ GeneradorLista.js
@@ -1,12 +1,7 @@
-class GeneradorLista{
-
-    constructor(){}
-
-    //Metodo que genera el texto(postgresql) necesario para hacer la busqueda por nombre
-    busquedaNombre(palabraClave){
-        var query ='Select * from Cancion where nombre_cacion  like ';
-        query + '%'+palabraClave+'%';
-        return query;
-    }
+//Metodo que genera el texto(postgresql) necesario para hacer la busqueda por nombre
+function busquedaNombre(palabraClave){
+    var query ='Select * from Cancion where nombre_cacion  like ';
+    query + '%'+palabraClave+'%';
+    return query;
 }
-export default GeneradorLista;
+module.exports.busquedaNombre = busquedaNombre;
