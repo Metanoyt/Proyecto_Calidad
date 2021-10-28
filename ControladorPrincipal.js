95f8af46bc01e89d2f8b41355077322d1aa831ea
KeylorCruz <42794277+KeylorCruz@users.noreply.github.com>
2021-10-27 18:58:21 -0600

    Correcion de conflictos
    Cambios en los imports y en la funcion de ControladorPrincipal

--- ControladorPrincipal.js
+++ ControladorPrincipal.js
@@ -1,4 +1,5 @@
 const {Client} = require('pg');
+const generador = require('./GeneradorLista.js');
 const login = require('./Login.js');
 
 const client = new Client ({
@@ -8,6 +9,20 @@ const client = new Client ({
     password: "4121",
     database: "postgres"
 })
+
+function requestByName(key){
+    client.connect();
+
+    client.query(generador.busquedaNombre(key),(err,res)=>{
+    
+        if(!err){
+            console.log(res.rows);
+        } else{
+            console.log(err.message);
+        }
+        client.end;
+    })
+}
 
 function Login(user,pass){
     client.connect();
